613ffcbf618894571c1717340065b4287edc10e2
Martin F. Schiffner <martin.schiffner@rub.de>
2019-03-21 23:20:48 +0100

    -added novel superclass +discretizations.grid

---
+++ +discretizations/grid.m
@@ -0,0 +1,57 @@
+%
+% superclass for all grids
+%
+% author: Martin F. Schiffner
+% date: 2019-03-21
+% modified: 2019-03-21
+%
+classdef grid
+
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	% properties
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	properties (SetAccess = private)
+
+        % independent properties
+        N_dimensions ( 1, 1 ) double { mustBeInteger, mustBePositive, mustBeNonempty } = 2              % number of dimensions (1)
+
+        % dependent properties
+
+    end % properties
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % methods
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	methods
+
+        %------------------------------------------------------------------
+        % constructor
+        %------------------------------------------------------------------
+        function objects = grid( N_dimensions )
+
+            %--------------------------------------------------------------
+            % 1.) check arguments
+            %--------------------------------------------------------------
+            % return for no input arguments
+            if nargin == 0
+                return;
+            end
+
+            %--------------------------------------------------------------
+            % 2.) create regular grids
+            %--------------------------------------------------------------
+            objects = repmat( objects, size( N_dimensions ) );
+
+            % check and set independent properties
+            for index_object = 1:numel( N_dimensions )
+
+                % set independent properties
+                objects( index_object ).N_dimensions = N_dimensions( index_object );
+
+            end % for index_object = 1:numel( N_dimensions )
+
+        end % function objects = grid( N_dimensions )
+
+    end % methods
+
+end % classdef grid
